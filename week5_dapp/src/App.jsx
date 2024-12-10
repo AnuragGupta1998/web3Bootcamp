766a1232475bc338201b1012f24f79db2d165d87
Anurag Gupta <anurag6967@gmail.com>
2024-12-10 22:52:40 +0530

    initialize react project for the DApp

--- week5_dapp/src/App.jsx
+++ week5_dapp/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='bg-green-400 h-fit w-full text-2xl'>App component</div>
+      <div className='bg-green-400 h-fit w-full text-2xl '>App component</div>
+      <div className='bg-yellow-400 text-2xl mt-1'> DApp building </div>
     </>
   )
 }
