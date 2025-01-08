01058548d91f89a340cae0426c6c5a4b8913b295
Anurag Gupta <anurag6967@gmail.com>
2025-01-08 17:02:35 +0530

    TokenLaunchpad.jsx frontend created

---
+++ week6.1_TokenLaunchpad/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TokenLaunchpad from './components/TokenLaunchpad'
+
+function App() {
+
+  return (
+    <>
+    hi there
+    <TokenLaunchpad />
+     
+    </>
+  )
+}
+
+export default App
