d0d927d0abeb41cf271b007e471a0e2d2fdd0d5d
Anurag Gupta <anurag6967@gmail.com>
2025-01-09 12:47:11 +0530

    installed required libraries and add wallet adapter component to app.jsx

--- week6.1_TokenLaunchpad/src/App.jsx
+++ week6.1_TokenLaunchpad/src/App.jsx
@@ -1,18 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import TokenLaunchpad from './components/TokenLaunchpad'
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+
+// Default styles that can be overridden by your app
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+import "./App.css";
+import TokenLaunchpad from "./components/TokenLaunchpad";
 
 function App() {
-
   return (
     <>
-    hi there
-    <TokenLaunchpad />
-     
+      <div className="bg-black rounded-t-xl ">
+        <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+          <WalletProvider wallets={[]} autoConnect>
+            <WalletModalProvider>
+              <div className="flex justify-between gap-x-5 mb-20">
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+              <TokenLaunchpad />
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
