07c9cd37a2a273487eac22785601bb4aab5a7a12
Anurag Gupta <anurag6967@gmail.com>
2025-02-09 14:04:35 +0530

    solana token launchpad with metadata

---
+++ week8.3_Solana_Token_Launchpad_With_Metadata/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-amber-200 text-2xl text-center font-bold text-black'>Solana Launchpad</h1>
+      <h2 className='bg-red-200 text-center '>hi</h2>
+      <h3 className='bg-'></h3>
+    </>
+  )
+}
+
+export default App
