daefc9f54ee1acdbc7ee4a50826b050946e4cf9e
Anurag Gupta <anurag6967@gmail.com>
2025-04-04 17:24:14 +0530

    use tanstack and view library to implement ethereum blockchain

--- week11_TanStackQueryEthWalletAdapter/src/App.jsx
+++ week11_TanStackQueryEthWalletAdapter/src/App.jsx
@@ -3,15 +3,26 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import FetchDataWeb2 from "./components/FetchDataWeb2";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Post from "./components/TenstackPost";
+import Viem from "./components/Viem";
 
 function App() {
+  const queryClient = new QueryClient();
 
   return(
   <>
-  <div>
-    <h1> How Data fetched in web2</h1>
-    <FetchDataWeb2 />
-  </div>
+  {/* for TanstackPost */}
+  {/*  <QueryClientProvider client={new QueryClient()}> */}
+  <QueryClientProvider client={queryClient}>
+
+    <Post />
+
+
+   </QueryClientProvider> 
+
+   {/* this is viem library */}
+  <Viem />
 
   </>
   )
